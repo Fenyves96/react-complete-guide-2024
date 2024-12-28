f255b31ce315710fef2bd2bf274f753e08c7e38d
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2024-12-28 17:37:14 +0100

    import export concepts

---
+++ JavaScript Refresher/starting-js/00-starting-js-proj/assets/scripts/app.js
@@ -0,0 +1,10 @@
+import { apiKey as content } from "./util.js";
+//import defaultValue from "./util.js";
+import * as util from "./util.js";
+
+
+console.log(util.apiKey);
+console.log(util.apiKey2);
+console.log(util.default);
+
+console.log(content);
