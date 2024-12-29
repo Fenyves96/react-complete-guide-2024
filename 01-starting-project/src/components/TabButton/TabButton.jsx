510fb6ccce88166ae7eded99bd0cb7ab4a2631f2
Fenyvesvölgyi Máté <22444404+Fenyves96@users.noreply.github.com>
2024-12-29 16:32:15 +0000

    handle click Event

--- 01-starting-project/src/components/TabButton/TabButton.jsx
+++ 01-starting-project/src/components/TabButton/TabButton.jsx
@@ -1,7 +1,11 @@
 export default function TabButton({ children }) {
+  function handleClick() {
+    console.log("HEllo word");
+  }
+
   return (
     <li>
-      <button>{children}</button>
+      <button onClick={handleClick}>{children}</button>
     </li>
   );
 }
