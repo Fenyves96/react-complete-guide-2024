aacc01b34819db9c6e0b667f3c35d10d1e97fd48
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2025-01-05 00:44:18 +0100

    two way binding

--- 03-tic-tac-toe/src/components/Player.jsx
+++ 03-tic-tac-toe/src/components/Player.jsx
@@ -1,23 +1,31 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing); //!isEditing cause unexpected behaviour
     //setIsEditing(!isEditing); sceduel a state update only
   }
+
+  function handleChange(event){
+    setPlayerName(event.target.value);
+  }
+
   let buttonCaption = 'Edit';
-  let playerName = <span className='player-name'>{name}</span>;
+  let editablePlayerName = <span className='player-name'>{playerName}</span>;
   if(isEditing){
-    playerName = <input type="text" required value={name} />;
+    editablePlayerName = <input
+     type="text" required value={playerName}
+     onChange={handleChange}/>;
     buttonCaption = 'Save';
   }
 
   return (
     <li>
       <span className="player">
-      {playerName}
+      {editablePlayerName}
 
         <span className='player-symbol'>{symbol}</span>
       </span>
