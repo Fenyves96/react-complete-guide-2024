47442384743cd9c7c04587057ce96461004c0182
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2025-01-04 21:30:27 +0100

    change Player layout based on isEditing

--- 03-tic-tac-toe/src/components/Player.jsx
+++ 03-tic-tac-toe/src/components/Player.jsx
@@ -1,11 +1,27 @@
-export default function Player({name, symbol}) {
-    return(
-        <li>
-        <span className="player">
-          <span className='player-name'>{name}</span>
-          <span className='player-symbol'>{symbol}</span>
-        </span>
-        <button>Edit</button>
-      </li>
-    );
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing); //!isEditing cause unexpected behaviour
+    //setIsEditing(!isEditing); sceduel a state update only
+  }
+  let buttonCaption = 'Edit';
+  let playerName = <span className='player-name'>{name}</span>;
+  if(isEditing){
+    playerName = <input type="text" required value={name} />;
+    buttonCaption = 'Save';
+  }
+
+  return (
+    <li>
+      <span className="player">
+      {playerName}
+
+        <span className='player-symbol'>{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{buttonCaption}</button>
+    </li>
+  );
 }
