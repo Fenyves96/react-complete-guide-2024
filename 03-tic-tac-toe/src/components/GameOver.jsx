020c2a6b2d2c657e10f859beb5d2b198727a65f0
fenyvesvolgyimate <mate.fenyvesvolgyi@lechnerkozpont.hu>
2025-01-05 14:23:37 +0100

    Game over screen

---
+++ 03-tic-tac-toe/src/components/GameOver.jsx
@@ -0,0 +1,12 @@
+export default function GameOver({ winner }) {
+  return (
+    <div id="game-over">
+      <h2>Game Over!</h2>
+      {winner &&<p>The winner is: {winner}</p>}
+      {!winner && <p>It's a draw!</p>}
+      <p>
+        <button>Play Again</button>
+      </p>
+    </div>
+  );
+}
